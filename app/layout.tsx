91dd6fc79826fcf829adbf8a4352be53529b274e
Prajapati_Darshan <vp941334@gmail.com>
2024-11-17 09:55:09 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({ subsets: ["latin"],
+  weight:['400','500','600','700'],
+  variable:'--font-poppins'
+
+});
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Where Crafting Jayous Occasions",
+  icons:{
+    icon:'/assets/images/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
