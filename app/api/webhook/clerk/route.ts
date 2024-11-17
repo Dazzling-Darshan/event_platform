49784efcb150e05673ec1a70cdf77beb311cab3c
Prajapati_Darshan <vp941334@gmail.com>
2024-11-17 10:12:23 +0530

    update project

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { createClerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { clerkClient, createClerkClient, WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
-import { clerkClient } from '@clerk/nextjs'
 
 
 export async function POST(req: Request) {
