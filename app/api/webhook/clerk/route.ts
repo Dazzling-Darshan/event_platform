c897842697b984bb2d3b53eda40418693c891002
Prajapati_Darshan <vp941334@gmail.com>
2024-11-17 10:19:30 +0530

    update project

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { clerkClient, createClerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 
@@ -67,14 +68,15 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
-    if(newUser)
-    {
-        await clerkClient.users.updateUserMetadata(id,{
-            publicMetadata: {
-                userId: newUser._id 
-            }
-        })
+    if (newUser) {
+      clerkClient().then((clerk) => {
+        clerk.users.updateUserMetadata(id, {
+          publicMetadata: { userId: newUser._id },
+        });
+      });
     }
+    
+    
     return NextResponse.json({ message:'OK', user: newUser})
   }
 
